18a440f457b03c5a768184cf74ac904995ee8fe8
mute <lewis.paul@live.co.uk>
2020-11-30 18:06:34 +0000

    Main Menu Update +

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
