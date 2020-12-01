ff43029c1097c40b2cde6dd5d3b70b9a74a54385
mute <lewis.paul@live.co.uk>
2020-12-01 06:57:49 +0000

    Updated Main Menu [Working]

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void playGame() {
+      Debug.Log("You're trying to play the game");
         SceneManager.LoadScene("SampleScene");
     }
 
     public void exitGame()
     {
+        Debug.Log("You're trying to quit the game");
         Application.Quit();
     }
 }
