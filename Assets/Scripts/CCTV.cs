4384b0726ddcc81f51c84f035fcbd320ab01ca0f
BCDeshiG <bcdg55@gmail.com>
2020-12-03 15:25:50 +0000

    Update CCTV.cs
    Prevent cameras from rotating whilst paused.

--- Assets/Scripts/CCTV.cs
+++ Assets/Scripts/CCTV.cs
@@ -4,39 +4,33 @@ using UnityEngine;
 
 public class CCTV : MonoBehaviour
 {
-    //Variable for CCTV movement and speed
-    public bool swap = true;
+    // Variables for CCTV movement and speed
+    private int swap = -1;
     public float start;
     public float end;
     public float rotationSpeed = 0.5f;
 
-    //Update is called once per frame
+    // Update is called once per frame
     private void Update()
     {
-        //Sets the boundaries where the camera needs to bounce
+        // Sets the boundaries where the camera needs to bounce
         if (transform.rotation.eulerAngles.z <= start)
         {
-            swap = !swap;
+            swap *= -1;
         }
         if (transform.rotation.eulerAngles.z >= end)
         {
-            swap = !swap;
+            swap *= -1;
         }
-        //Changes the clockwise or anticlockwise motion of the camera
-        if (swap)
-        {
-            transform.Rotate(Vector3.forward * -rotationSpeed);
-        }
-        else
-        {
-            transform.Rotate(Vector3.forward * rotationSpeed);
-        }
+        // Changes the clockwise or anticlockwise motion of the camera
+        transform.Rotate(Vector3.forward * swap * rotationSpeed * Time.timeScale);
+
     }
 
-    //Triggered when something enters the CCTV cone of vision
+    // Triggered when something enters the CCTV cone of vision
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //If the cone finds the player then they are sent to start
+        // If the cone finds the player then they are sent to start
         if (collision.tag == "Player")
         {
             PlayerController player = collision.GetComponent<PlayerController>();
