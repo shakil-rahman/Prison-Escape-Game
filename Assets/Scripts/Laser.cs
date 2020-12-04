6944ac53570cd09becd40d599782fe6ed15a0835
BCDeshiG <bcdg55@gmail.com>
2020-12-04 17:21:54 +0000

    Lasers + Sound Tweaks
    Fixed footsteps sound, trimmed bgm so it loops properly, added lasers that damage player on contact and flicker randomly.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // How long the laser is turned on for
+    private float activeLength;
+    // How long the laser waits between activations
+    public float waitLength;
+    // The amount of time the laser has waited
+    private float timeGap = 0;
+    // Stores if laser is active
+    private bool isActive = true;
+    // Store references to objects
+    public GameObject laser;
+    public GameObject laserLight;
+    // Coroutine for laser damage
+    private IEnumerator coroutine;
+    private bool damaging = false;
+
+    void Start()
+    {
+        activeLength = Random.Range(2, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(damaging);
+        timeGap += Time.fixedDeltaTime;
+        if(timeGap > activeLength)
+        {
+            if(timeGap > (activeLength + waitLength))
+            {
+                timeGap = 0;
+                isActive = true;
+                laser.GetComponent<Renderer>().enabled = true;
+                laserLight.SetActive(true);
+            }
+            else
+            {
+                isActive = false;
+                laser.GetComponent<Renderer>().enabled = false;
+                laserLight.SetActive(false);
+            }
+        }
+    }
+    
+    // Detect if touching laser
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && isActive)
+        {
+            coroutine = laserDamage();
+            StartCoroutine(coroutine);
+        }
+    }
+
+    private IEnumerator laserDamage()
+    {
+        // Prevents damage during cooldown
+        if(!damaging)
+        {
+            damaging = true;
+            HealthManager.damage(1);
+            yield return new WaitForSeconds(1);
+            damaging = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,75 +5,77 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variable for movement and animation
+    // Variable for movement and animation
     public float moveSpeed = 6.5f;
     public Rigidbody2D rb;
     public Animator anim;
-    //Store all the keys collected
+    // Store all the keys collected
     public static List<string> keys;
-    //Vectors to store location and movement
+    // Vectors to store location and movement
     Vector2 movement;
     Vector3 start;
 
     public AudioSource walkingsound;
 
-    //Stores start position and adds "Cell" door
+    // Stores start position and adds "Cell" door
     private void Awake()
     {
         start = rb.position;
         keys = new List<string>();
-        //Allows prisoner to escape the first door
+        // Allows prisoner to escape the first door
         keys.Add("Cell");
     }
 
-    //Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        //Retrieves the movement value on x and y axis
+        // Retrieves the movement value on x and y axis
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetKeyDown("w") || Input.GetKeyDown("s") || Input.GetKeyDown("a")|| Input.GetKeyDown("d"))
+        // Plays footstep sound if moving
+        if(movement.x != 0 || movement.y != 0)
         {
-          walkingsound.Play();
-
+            // Prevents sound from overlapping
+            if(!walkingsound.isPlaying)
+            {
+                walkingsound.Play();
+            }
         }
-
-        else if(Input.GetKeyUp("w")||Input.GetKeyUp("a")||Input.GetKeyUp("s")||Input.GetKeyUp("d"))
+        else
         {
           walkingsound.Stop();
         }
 
-
-        //Changes the animation based on the movement value
+        // Changes the animation based on the movement value
         anim.SetFloat("speedX", movement.x);
         anim.SetFloat("speedY", movement.y);
         anim.SetFloat("speed", movement.sqrMagnitude);
     }
 
-    //Moves the player at a constant rate
+    // Moves the player at a constant rate
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
-    //Returns player to the start point
+    // Returns player to the start point
     public void respawn()
     {
         rb.position = start;
     }
 
-    //Triggered when player enters another collider
+    // Triggered when player enters another collider
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Checks the key, adds it to the list then destroys it
+        // Checks the key, adds it to the list then destroys it
         Key key = collision.GetComponent<Key>();
         if (key != null)
         {
             keys.Add(key.getKeyType());
             Destroy(key.gameObject);
         }
-        //Checks if we have the key for the door and opens it if we do
+        // Checks if we have the key for the door and opens it if we do
         Door door = collision.GetComponent<Door>();
         if (door != null)
         {
